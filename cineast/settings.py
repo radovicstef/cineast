3d7fd1d9be3a5bb23d6392d1bde6aa17bc64b4c2
Stefan Radovic <radovicstef@gmail.com>
2021-11-03 10:38:52 +0100

    Added mySQL configuration

--- cineast/settings.py
+++ cineast/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'cineast.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cineast_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
